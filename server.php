de798bf71492f69b9e990c39f6b21cacf38a73c0
MaDGiiRL <sofiavidotto8@gmail.com>
2025-11-11 12:32:53 +0100

    fix

--- server.php
+++ server.php
@@ -1,13 +1,9 @@
 <?php
-
-// Serve gli asset statici direttamente
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-$publicPath = __DIR__ . '/public';
-$filePath = $publicPath . $uri;
+$public = __DIR__ . '/public';
+$file = $public . $uri;
 
-if ($uri !== '/' && file_exists($filePath) && is_file($filePath)) {
-    return readfile($filePath);
+if ($uri !== '/' && file_exists($file) && is_file($file)) {
+    return readfile($file);
 }
-
-// Bootstrap Laravel come fa public/index.php
-require $publicPath . '/index.php';
+require $public . '/index.php';
